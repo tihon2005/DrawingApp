70eba9097ccda2c76e12550f26a7177edb0cfffd
Tsikhon Bazar <103754328+tihon2005@users.noreply.github.com>
2023-08-12 23:18:05 +0300

    Initial Commit

---
+++ DrawingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by Tikhon Bazar on 12.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
